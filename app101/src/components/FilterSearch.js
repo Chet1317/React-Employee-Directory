f82a46506ba7f00e0bac79abd9f51b036515c383
Achilles <63617922+Chet1317@users.noreply.github.com>
2020-07-28 19:56:09 -0400

    created function for searching under different filter

--- app101/src/components/FilterSearch.js
+++ app101/src/components/FilterSearch.js
@@ -1,14 +1,23 @@
 import React from "react"
 
 function FilterSearch(props){
+
+  function searchDifferentFilter( event ){
+    const { name, value }= event.target
+   
+
+    props.updateFilter( value )
+}
+
     return(
         <div class="form-group">
         <label for="exampleFormControlSelect1">Filter</label>
-        <select class="form-control" id="exampleFormControlSelect1">
-          <option>Name</option>
-          <option>Email</option>
-          <option>Phone Number</option>
-          <option>Position</option>
+        <select onSearch={searchDifferentFilter} value={props.filter} class="form-control" id="exampleFormControlSelect1">
+        <option value="id">id</option>
+          <option value="name">Name</option>
+          <option value="email">Email</option>
+          <option value="phone">Phone Number</option>
+          <option value="position">Position</option>
         </select>
       </div>
     )
