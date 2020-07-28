cc1a880efa23d9b7545c0a93399b9d2981d5caa6
Achilles <63617922+Chet1317@users.noreply.github.com>
2020-07-28 14:57:15 -0400

    added employees.json file for table

--- app101/src/components/EmployeeList.js
+++ app101/src/components/EmployeeList.js
@@ -6,6 +6,7 @@ function EmployeeList (props){
         <table class="table">
         <thead>
           <tr>
+            <th scope="col">#</th>
             <th scope="col">Name</th>
             <th scope="col">Email</th>
             <th scope="col">Phone Number</th>
@@ -14,18 +15,21 @@ function EmployeeList (props){
         </thead>
         <tbody>
           <tr>
+          <th scope="col">1</th>
             <th scope="row">Rich Froning</th>
             <td>rich@gmail.com</td>
             <td>905-999-4278</td>
             <td>Accountant</td>
           </tr>
           <tr>
+          <th scope="col">2</th>
             <th scope="row">Jessica Williams</th>
             <td>j_williams@gmail.com</td>
             <td>705-289-3389</td>
             <td>Sales Representative</td>
           </tr>
           <tr>
+          <th scope="col">3</th>
             <th scope="row">Bruce Wayne</th>
             <td>brucew@gmail.com</td>
             <td>999-678-4519</td>
