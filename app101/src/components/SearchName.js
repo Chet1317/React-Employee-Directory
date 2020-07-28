c03605625f685138d88de79ad2bd055b7a5ce599
Achilles <63617922+Chet1317@users.noreply.github.com>
2020-07-28 14:01:37 -0400

    added the rest of html components and created components folder

---
+++ app101/src/components/SearchName.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function SearchName (props){
+    return (
+        <div class="form-group">
+        <label for="name">Search</label>
+        <input type="password" class="form-control" id="name" placeholder="Name"></input>
+      </div>
+    )
+}
+
+export default SearchName
